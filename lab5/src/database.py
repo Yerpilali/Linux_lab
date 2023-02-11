54c7722f6232c864f0a8818ced9e989f757d4e82
Daniil Nesterov <daniilnesterov15@mail.ru>
2023-02-11 12:01:36 +0500

    Add files via upload

---
+++ lab5/src/database.py
@@ -0,0 +1,9 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./sqlite_base.db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={}
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
